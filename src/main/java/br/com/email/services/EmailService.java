a2fc46bc706e02b7a737077a87acaf4042604e6f
luiz fernando <luiz123jfmg@gmail.com>
2021-11-24 12:31:57 -0300

    Implementando o disparo do email

--- src/main/java/br/com/email/services/EmailService.java
+++ src/main/java/br/com/email/services/EmailService.java
@@ -1,9 +1,15 @@
 package br.com.email.services;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import br.com.email.entities.Email;
+import br.com.email.enums.StatusEmail;
 import br.com.email.repositories.EmailRepository;
 
 @Service
@@ -12,9 +18,30 @@ public class EmailService {
 	
 	@Autowired
 	EmailRepository repository;
+	
+	@Autowired
+	private JavaMailSender emailSender;
 
-	public void sendEmail(Email email) {
-		// TODO Auto-generated method stub
+	@SuppressWarnings("finally")
+	public Email sendEmail(Email email) {
+		email.setSendDateEmail(LocalDateTime.now());
+		
+		try {
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setFrom(email.getEmailFrom());
+			message.setTo(email.getEmailTo());
+			message.setSubject(email.getSubject());
+			message.setText(email.getText());
+			emailSender.send(message);
+			
+			email.setStatusEmail(StatusEmail.SENT);			
+		}catch (MailException e) {
+			email.setStatusEmail(StatusEmail.ERROR);
+		}finally {
+			return repository.save(email);
+		}
+		
+		
 		
 	}
 	
